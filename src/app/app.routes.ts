f3c64b67c4b42e75738198cb085e1ec22cef53ef
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-03-17 20:48:06 -0300

    criando e desenvolvendo o componente list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './modules/lista-tarefa/pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+];
